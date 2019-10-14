e0bfe50aaf8ece646997127385e9c10e23434546
Aditya Athalye <aditya.athalye@gmail.com>
2019-10-14 23:11:30 +0530

    Script some basic tests for game victory logic

---
+++ oxo_test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+source "./oxo.sh"
+
+function set_board_to_values {
+    local __values=${1}
+
+    for i in $(seq ${board_size})
+    do set_pos_to_val ${i} "$(printf "$__values" | cut -d ' ' -f ${i})"
+    done
+}
+
+victory_X_row="O X O X X X X X O"
+victory_O_row="O X O O O O X X O"
+
+victory_X_col="X X O X X O X X O"
+victory_O_col="O O X O O X O O X"
+
+victory_diagonal_Xs="X X O O X O X O X"
+victory_diagonal_Os="O X X O O X X O O"
+
+set_board_to_values "$victory_X_row";
+if __player_wins_grid_row __all_crosses
+then echo "Player won Row Xs!"
+fi
+
+set_board_to_values "$victory_O_row";
+if __player_wins_grid_row __all_noughts
+then echo "Player won Row Os!"
+fi
+
+set_board_to_values "$victory_X_col";
+if __player_wins_grid_col __all_crosses
+then echo "Player won Col Xs!"
+fi
+
+set_board_to_values "$victory_O_col";
+if __player_wins_grid_col __all_noughts
+then echo "Player won Col Os!"
+fi
+
+set_board_to_values "$victory_diagonal_Xs";
+if __player_wins_grid_left_diagonal __all_crosses
+then echo "Player won Left Diagonal Xs!"
+fi
+
+set_board_to_values "$victory_diagonal_Os";
+if __player_wins_grid_left_diagonal __all_noughts
+then echo "Player won Left Diagonal Os!"
+fi

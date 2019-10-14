82bb4d2aec445b0f06f23c19f265c1041b8a5256
Aditya Athalye <aditya.athalye@gmail.com>
2019-10-14 11:07:40 +0530

    Init noughts+crosses for 2-player, 3x3 board game

---
+++ oxo.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# ########################################
+# Traditional Noughts and Crosses
+#
+# - 3x3 board with 2 players
+# - player 1 is human
+# - player 2 can be human or computer
+# - Any full diagonal or horizontal wins
+# - First player starts with x always
+#
+# ########################################

1094348dc9e0eac25fd12da04f5294c00fe1a5ed
Aditya Athalye <aditya.athalye@gmail.com>
2019-10-14 20:55:52 +0530

    Add globals & fns to define the board's design
    All oxo boards are squares, and the board state is an array of:
    empty  = - (hyphen)
    cross  = X (upper case x)
    nought = O (upper case o)
    
    A lot of logic would get built around fit_board_to_square_grid, as we
    will be able to play to the strengths of bash text processing + pattern
    matching / REGEXP-fu. For example, a player wins a game if the pattern
    of _any row_ is "X X X" or "O O O". This is simply a grep with return 0:
    
        grep -E "^X[[:space:]]+X[[:space:]]+X$"
    
    To check if any a column is full; we can transpose the board, and
    perform the grep against the transposed board.
    
    To check if a diagonal is full; we can extract (say items 1,5,9 from the
    game_state array) and once again grep against the same pattern.

--- oxo.sh
+++ oxo.sh
@@ -10,3 +10,51 @@
 # - First player starts with x always
 #
 # ########################################
+
+
+# ########################################
+# BOARD DESIGN
+# ########################################
+
+# All oxo boards are squares, and the board state is an array of:
+# empty  = - (hyphen)
+# cross  = X (upper case x)
+# nought = O (upper case o)
+declare -a board_state
+declare -r square_side=3
+declare -r board_size=$(( square_side * square_side ))
+
+function set_pos_to_val {
+    local pos=${1}
+    local val=${2}
+
+    board_state[${pos}]=${val}
+}
+
+function reset_board {
+    for i in $(seq ${board_size});
+    do set_pos_to_val ${i} "-";
+    done
+}
+
+function fit_val_to_square_grid {
+    printf "%s " ${1};
+}
+
+function fit_board_to_square_grid {
+    local num_grid_columns=$(( ${square_side} * 2 ))
+
+    for val in ${board_state[*]}; do fit_val_to_square_grid ${val} ; done \
+        | fmt -w ${num_grid_columns} -
+}
+
+# We display based on the square grid
+function display_oxo_board {
+    local row_labels='\n\n\n\ta\n\tb\n\tc'
+    local col_labels='\n\t1 2 3\n\n'
+
+    paste <(printf ${row_labels}) \
+          <(cat <(printf "$col_labels") \
+                <(fit_board_to_square_grid) \
+                <(printf "\n"))
+}
